cd0feaf18477e040cf2f0309b72666939a1db98e
Michael Adamski <mikeComp@Michaels-MacBook-Pro.local>
2017-02-27 15:21:29 -0500

    Added FizzBuzz Challenge

---
+++ fizz.rb
@@ -0,0 +1,14 @@
+counter = 0
+
+while counter < 100 do
+  counter += 1
+    if (counter % 3 == 0) && (counter % 5 == 0)
+      puts "FizzBuzz"
+    elsif counter % 5 == 0
+      puts "Buzz ----"
+    elsif counter % 3 == 0
+      puts "Fizz ----"
+    else
+      puts counter
+end
+end
